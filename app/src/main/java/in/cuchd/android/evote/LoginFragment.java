929141d494273217cd53ba08ed482d60ffadb982
AK <anuj.anonymous@gmail.com>
2022-03-17 16:14:16 +0530

    Add LoginFragment

---
+++ app/src/main/java/in/cuchd/android/evote/LoginFragment.java
@@ -0,0 +1,26 @@
+package in.cuchd.android.evote;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class LoginFragment extends Fragment
+{
+    public static Fragment newInstance()
+    {
+        return new LoginFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        return view;
+    }
+}
