272e152b7642fbc1504ea7426ec97790f10e2bb5
AK <anuj.anonymous@gmail.com>
2022-03-24 19:34:25 +0530

    Move login tab and greeting logic from MainActivity to LoginFragment

--- app/src/main/java/in/cuchd/android/evote/LoginFragment.java
+++ app/src/main/java/in/cuchd/android/evote/LoginFragment.java
@@ -4,13 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class LoginFragment extends Fragment
 {
+    private TabLayout mLoginTab;
+    private TextView mGreeting;
+
     public static Fragment newInstance()
     {
         return new LoginFragment();
@@ -21,6 +27,47 @@ public class LoginFragment extends Fragment
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        mLoginTab = view.findViewById(R.id.login_tab);
+        mLoginTab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener()
+        {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab)
+            {
+                updateGreeting();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab)
+            {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab)
+            {
+
+            }
+        });
+
+        mGreeting = view.findViewById(R.id.greet_text_view);
+
+        updateGreeting();
+
         return view;
+    }
+
+    private void updateGreeting()
+    {
+        int selectedPosition = mLoginTab.getSelectedTabPosition();
+
+        if (selectedPosition == 0)
+        {
+            mGreeting.setText(getString(R.string.greet_text, "voter"));
+        }
+        else
+        {
+            mGreeting.setText(getString(R.string.greet_text, "commissioner"));
+        }
     }
 }
--- app/src/main/java/in/cuchd/android/evote/MainActivity.java
+++ app/src/main/java/in/cuchd/android/evote/MainActivity.java
@@ -1,50 +1,20 @@
 package in.cuchd.android.evote;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import android.os.Bundle;
-import android.widget.TextView;
-
-import com.google.android.material.tabs.TabLayout;
-
 public class MainActivity extends AppCompatActivity
 {
     private static final String TAG = "MAIN_ACTIVITY";
-
-    private TabLayout mLoginTab;
-    private TextView mGreeting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mLoginTab = findViewById(R.id.login_tab);
-        mLoginTab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener()
-        {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab)
-            {
-                updateGreeting();
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab)
-            {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab)
-            {
-
-            }
-        });
-
-        mGreeting = findViewById(R.id.greet_text_view);
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.login_container);
@@ -54,22 +24,6 @@ public class MainActivity extends AppCompatActivity
         {
             fragment = LoginFragment.newInstance();
             fm.beginTransaction().add(R.id.login_container, fragment).commit();
-        }
-
-        updateGreeting();
-    }
-
-    private void updateGreeting()
-    {
-        int selectedPosition = mLoginTab.getSelectedTabPosition();
-
-        if (selectedPosition == 0)
-        {
-            mGreeting.setText(getString(R.string.greet_text, "voter"));
-        }
-        else
-        {
-            mGreeting.setText(getString(R.string.greet_text, "commissioner"));
         }
     }
 }
