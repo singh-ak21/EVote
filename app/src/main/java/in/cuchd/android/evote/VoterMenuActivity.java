c113a74009852f3445a9d17878bb89b401fca7d2
AK <anuj.anonymous@gmail.com>
2022-03-24 19:51:30 +0530

    Add VoterMenuActivity.java

---
+++ app/src/main/java/in/cuchd/android/evote/VoterMenuActivity.java
@@ -0,0 +1,15 @@
+package in.cuchd.android.evote;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VoterMenuActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.voter_menu);
+    }
+}
