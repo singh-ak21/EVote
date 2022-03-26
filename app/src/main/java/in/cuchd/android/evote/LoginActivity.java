f16f539e0be9de348cacc95572fe2ee72648b45a
AK <anuj.anonymous@gmail.com>
2022-03-26 11:30:38 +0530

    Refactor LoginActivity to use SingleFragmentActivity

--- app/src/main/java/in/cuchd/android/evote/LoginActivity.java
+++ app/src/main/java/in/cuchd/android/evote/LoginActivity.java
@@ -1,29 +1,14 @@
 package in.cuchd.android.evote;
 
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
-public class LoginActivity extends AppCompatActivity
+public class LoginActivity extends SingleFragmentActivity
 {
     private static final String TAG = "MAIN_ACTIVITY";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
+    protected Fragment createFragment()
     {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_container);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.login_container);
-
-        // load the login fragment layout
-        if (fragment == null)
-        {
-            fragment = LoginFragment.newInstance();
-            fm.beginTransaction().add(R.id.login_container, fragment).commit();
-        }
+        return LoginFragment.newInstance();
     }
 }
