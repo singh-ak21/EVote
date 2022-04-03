65887f846d2a74fadb92d31fd660aae37f83bf1f
20BCS3625 <20BCS3625@cuchd.in>
2022-04-03 20:58:43 +0530

    Update login activity

--- app/src/main/java/in/cuchd/android/evote/LoginActivity.java
+++ app/src/main/java/in/cuchd/android/evote/LoginActivity.java
@@ -1,5 +1,7 @@
 package in.cuchd.android.evote;
 
+import android.content.Intent;
+
 import androidx.fragment.app.Fragment;
 
 public class LoginActivity extends AbstractMainActivity
@@ -7,7 +9,7 @@ public class LoginActivity extends AbstractMainActivity
     private static final String TAG = "MAIN_ACTIVITY";
 
     @Override
-    protected Fragment createFragment()
+    protected Fragment createFragment(Intent bundle)
     {
         return LoginFragment.newInstance();
     }
