e2897359a8e0b6eef23eb511ff516cbcc2aff099
20BCS3625 <20BCS3625@cuchd.in>
2022-04-03 11:17:57 +0530

    Configure sign up and verify otp files

--- app/src/main/java/in/cuchd/android/evote/SignupFragment.java
+++ app/src/main/java/in/cuchd/android/evote/SignupFragment.java
@@ -1,9 +1,11 @@
 package in.cuchd.android.evote;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,6 +13,8 @@ import androidx.fragment.app.Fragment;
 
 public class SignupFragment extends Fragment
 {
+    private Button mButton;
+
     public static Fragment newInstance()
     {
         return new SignupFragment();
@@ -19,6 +23,18 @@ public class SignupFragment extends Fragment
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_signup, container, false);
+
+        mButton = view.findViewById(R.id.continue_button);
+        mButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(getActivity(), VerifyOTPActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
 }
