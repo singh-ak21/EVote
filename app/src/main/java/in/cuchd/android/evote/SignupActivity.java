52375467b8229bf15dbb30664ddece221154c330
20BCS3625 <20BCS3625@cuchd.in>
2022-04-03 20:57:46 +0530

    Update files sign up contents

--- app/src/main/java/in/cuchd/android/evote/SignupActivity.java
+++ app/src/main/java/in/cuchd/android/evote/SignupActivity.java
@@ -1,11 +1,13 @@
 package in.cuchd.android.evote;
 
+import android.content.Intent;
+
 import androidx.fragment.app.Fragment;
 
 public class SignupActivity extends AbstractMainActivity
 {
     @Override
-    protected Fragment createFragment()
+    protected Fragment createFragment(Intent bundle)
     {
         return SignupFragment.newInstance();
     }
